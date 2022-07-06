e80113e0d14efb8a3473cc1e869937303c2fad1d
Minh Tran <minhtran.2404@outlook.com>
2022-07-06 21:25:58 +0700

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,27 @@
+# %%
+from simple import *
+from contentBased import *
+from collaborativeBased import *
+from hybrid import *
+
+# %% [markdown]
+# ## Simple Recommender
+simpleRec = SimpleRecommender()
+simpleRec.build_chart('Romance').head(10)
+
+# %% [markdown]
+# ## Content-based Recommender
+contentRec = ContentBasedRecommender()
+contentRec.prepareMetadataBased()
+contentRec.improved_recommendations('The Dark Knight').head(10)
+
+
+# %% [markdown]
+# ## Collaborative-based Recommender
+collabRec = CollaborativeBasedRecommender()
+
+# %% [markdown]
+# ## Hybrid Recommender
+hybridRec = HybridRecommender()
+hybridRec.hybrid(1, 'Mean Girls')
+
